5a366d5fd0a2fc7e20c52a67773e9c04a9b632d2
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-09-07 12:23:42 +0600

    lesson card added

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const loadLesson = () =>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res)=> res.json())
+    .then( (json) => displayLesson(json.data))
+}
+
+const loadLevelWord = (id) =>{
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>displayLevelWord(data.data))
+}
+
+// "id": 6,
+// "level": 5,
+// "word": "Fascinate",
+// "meaning": "মুগ্ধ করা",
+// "pronunciation": "ফ্যাসিনেট"
+
+const displayLevelWord = (words) => {
+ 
+   const wordContainer = document.getElementById("word-container")
+   wordContainer.innerHTML = " ";
+   words.forEach((word)=> {
+    const card = document.createElement('div');
+    card.innerHTML = `
+       <div class="bg-white text-center p-10 rounded-xl">
+        <h2 class="text-3xl font-bold">${word.word}</h2>
+        <p class="text-xl mt-2 mb-3">Meaning /Pronounciation</p>
+        <div class="font-semibold text-3xl font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="flex justify-between mt-6">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
+    
+    `
+    wordContainer.append(card)
+   })
+}
+const displayLesson = (lessons) => {
+
+    const lessonContainer = document.getElementById("lesson-container")
+    lessonContainer.innerHTML = " ";
+    
+    for(let lesson of lessons){
+        
+        const btnDiv = document.createElement("div")
+        btnDiv.innerHTML = `
+                <button onClick=loadLevelWord(${lesson.level_no}) class="btn btn-soft btn-primary">
+                    <i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}
+                </button>
+        `
+        lessonContainer.append(btnDiv)
+    }
+
+}
+
+loadLesson();
